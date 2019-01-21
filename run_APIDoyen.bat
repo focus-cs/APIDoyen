9fb45bd2a4cdaef2a0cf95a5e059e1b32e3285e1
lahoudie <lahoudie.eric@gmail.com>
2019-01-21 21:47:03 +0100

    Add files via upload

---
+++ run_APIDoyen.bat
@@ -0,0 +1,33 @@
+set BATCH_PATH=D:/OneDrive/Documents/Professionnel/Doyen/API
+set SCIFORMA_URL=https://doyen-test.sciforma.net/sciforma/
+set BATCH_MAIN=APIDoyen.jar
+
+
+set ROOT_DIR=%BATCH_PATH%
+set LIB_DIR=%ROOT_DIR%/lib
+
+cd %LIB_DIR%
+
+IF EXIST "PSClient_en.jar" (
+    del "PSClient_en.jar"
+)
+IF EXIST "PSClient.jar" (
+    del "PSClient.jar"
+)
+IF EXIST "utilities.jar" (
+    del "utilities.jar"
+)
+
+wget.exe  -O utilities.jar %SCIFORMA_URL%/utilities.jar
+wget.exe  -O PSClient_en.jar %SCIFORMA_URL%/PSClient_en.jar
+wget.exe -O PSClient.jar %SCIFORMA_URL%/PSClient.jar
+
+cd %ROOT_DIR%
+
+set JAVA_ARGS=-showversion
+set JAVA_ARGS=%JAVA_ARGS% -Xms1024m
+set JAVA_ARGS=%JAVA_ARGS% -Xmx2048m
+set JAVA_ARGS=%JAVA_ARGS% -jar
+
+java %JAVA_ARGS% %BATCH_MAIN%
+ 
